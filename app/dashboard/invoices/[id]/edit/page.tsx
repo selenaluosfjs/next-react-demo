aff30ba81aa8332e789b6b65664207cbc14994b3
luohaiyan <614603971@qq.com>
2024-05-21 09:11:20 +0800

    功能

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById('126eed9c-c90c-4ef6-a4a8-fcf7408d3c66'),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
